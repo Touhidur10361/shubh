a3d12ffa3829ae02b565207636f5e6bad50ecfc4
Touhidur10361 <85865792+Touhidur10361@users.noreply.github.com>
2023-05-13 10:56:13 +0600

    pythonhnew.py

---
+++ new.py
@@ -0,0 +1,13 @@
+print("Welcome to the Quiz")
+print("What is the capital of BD?")
+print("1. Dhaka")
+print("2. Cumilla")
+print("3. Sylhet")
+choose=input("Enter 1 2 or 3 : ")
+
+if choose=="1":
+  print("Right Answer")
+elif choose=="2":
+  print("Wrong answer")
+elif choose=="3":
+  print("Wrong answer")
